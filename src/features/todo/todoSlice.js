d77d2fc51e61593b664ceb18ab15389a44e2dbf2
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-29 21:38:19 +0530

    Define TodoSlice with Initial State & Reducers

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    {
+      id: nanoid(),
+      text: "First Todo",
+    },
+  ],
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push({
+        id: nanoid(),
+        text: action.payload,
+      });
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
